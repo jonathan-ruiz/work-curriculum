be6fc370016cb9bbe1e6365d1ccad6c7d07c583a
Jonathan <jonathan.ruiz@bitheater.co.uk>
2024-03-08 11:28:14 +0100

    chore: move curriculum sections to components

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,7 @@
 </script>
 
 <template>
-  <header v-if="false" class="bg-gray-100 top-0 z-50 p-1 place-content-end grid">
-    <button
-        @click="theme = theme == 'default' ? 'high-contrast' : 'default'"
-        class=" bg-blue-100 dark:bg-blue-800 rounded-md p-2 m-2 hover:bg-blue-600 dark:hover:bg-blue-900"
-    >
-      <SunIcon class="w-6 inline-block"></SunIcon>
-      High Contrast
-    </button>
-  </header>
-  <Curriculum :theme="theme" :version="version as 'online' | 'printable'"/>
+  <Curriculum :version="version as 'online' | 'printable'"/>
 </template>
 
 <style scoped>
---
+++ src/components/PrintableHeader.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+export interface Props {
+  themeClass?: string,
+}
+const props = withDefaults(defineProps<Props>(), {
+  themeClass: '',
+})
+</script>
+<template>
+  <section v-if="isPrintable" class="pt-0" :class="[themeClass]" >
+
+    <!-- isPrintable header profile picture -->
+    <div v-if="!isPrintable" class="flex flex-wrap justify-center">
+      <div class="flex justify-center w-full">
+        <div class="relative">
+          <img src="../assets/images/profile-image-1.png"
+               class="dark:shadow-xl rounded-full align-middle border-4 max-w-[150px]"
+               :class="[themeClass]"
+               alt="profile image"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="mt-4 text-center" :class="isPrintable ? 'text-xs':''">
+      <h1>Jonathan Ruiz</h1>
+      <h2>Software Engineer</h2>
+      <span class="inline-block">
+              <a href="http://jonathanruiz.co.uk" class="text-inherit">jonathanruiz.co.uk</a>
+            </span>
+      <span class="inline-block">
+            +44 (0)7754 218 610
+            </span>
+      <span class="inline-block">
+            contact@jonathanruiz.co.uk
+            </span>
+    </div>
+  </section>
+</template>
